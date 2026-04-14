6850d6717ff017c97f7d0b59ee25904bbce2c3aa
Timothy Neymeyer <timothy.james.n@outlook.com>
2026-04-13 21:49:48 -0500

    Changed SCREENSIZE to RESOLUTION. Added draw method to Pacman class

--- src/project.py
+++ src/project.py
@@ -5,21 +5,24 @@ from constants import *
 
 class GameController(object):
 
-    def __init__(self):
-        pass
+    def __init__(self, screen, background):
+        self.screen = screen
+        self.background = background
 
     def startGame(self):
-        pass
+        self.pacman = Pacman()
 
-    def update(self):
+    def update(self, dt):
+        self.pacman.update(dt)
         self.checkEvents()
-        self.render()
+        self.draw()
 
     def checkEvents(self):
         pass
 
-    def render(self):
-        pass
+    def draw(self):
+        self.screen.blit(self.background, (0, 0))
+        self.pacman.draw(self.screen)
 
 class Pacman(object):
 
@@ -28,13 +31,13 @@ class Pacman(object):
         self.position = Vector2(200, 400)
         self.directions = {
             STOP:Vector2(),
-            UP: Vector2(0, -1),
+            UP:Vector2(0, -1),
             DOWN:Vector2(0, 1),
             LEFT:Vector2(-1, 0),
             RIGHT:Vector2(1, 0)
         }
         self.direction = STOP
-        self.speed = 100 #tutorial does 100 * TILEAREA/25 for some reason
+        self.speed = 100 #* TILEAREA/25
         self.radius = 10
         self.color = yellow
 
@@ -43,6 +46,30 @@ class Pacman(object):
         direction = self.getValidKey()
         self.direction = direction
 
+    def getValidKey(self):
+        key = pygame.key.get_pressed()
+        match key:
+            case [K_UP]:
+                print("up")
+                return UP
+            case [K_DOWN]:
+                print("down")
+                return DOWN
+            case [K_LEFT]:
+                print("left")
+                return LEFT
+            case [K_RIGHT]:
+                print("right")
+                return RIGHT
+            case _:
+                return STOP
+            
+    def draw(self, screen):
+        p = self.position.asIntTup()
+        pygame.draw.circle(screen, yellow, p, self.radius)
+
+
+
 def main():
     print("Hello World!")
     pygame.init()
@@ -50,11 +77,11 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
-    screen = pygame.display.set_mode(SCREENSIZE)
-    background = pygame.Surface(SCREENSIZE)
+    screen = pygame.display.set_mode(RESOLUTION)
+    background = pygame.Surface(RESOLUTION)
     background.fill(black)
 
-    game = GameController()
+    game = GameController(screen, background)
     game.startGame()
 
     running = True
@@ -64,8 +91,11 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
 
+        game.update(dt)
+
         pygame.display.flip()
         dt = clock.tick(30) / 1000.0
+    pygame.quit()
 
 if __name__ == "__main__":
     main()
