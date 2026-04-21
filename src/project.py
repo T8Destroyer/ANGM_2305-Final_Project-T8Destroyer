8b39402caf80688c4750fb9c8323b5bbd0c9d810
Timothy Neymeyer <timothy.james.n@outlook.com>
2026-04-20 20:26:42 -0500

    Added Node class and Maze class to make the moving space for pacman, and initialize a test maze, respectivley

--- src/project.py
+++ src/project.py
@@ -10,6 +10,8 @@ class GameController(object):
         self.background = background
 
     def startGame(self):
+        self.maze = Maze()
+        self.maze.setupTestNodes()
         self.pacman = Pacman()
 
     def update(self, dt):
@@ -22,6 +24,7 @@ class GameController(object):
 
     def draw(self):
         self.screen.blit(self.background, (0, 0))
+        self.maze.draw(self.screen)
         self.pacman.draw(self.screen)
 
 class Pacman(object):
@@ -62,7 +65,48 @@ class Pacman(object):
         p = self.position.asIntTup()
         pygame.draw.circle(screen, yellow, p, self.radius)
 
+class Node(object):
 
+    def __init__(self, x, y):
+        self.position = Vector2(x, y)
+        self.neighbors = {UP:None, DOWN:None, LEFT:None, RIGHT:None}
+
+    def draw(self, screen):
+        for i in self.neighbors.keys():
+            if self.neighbors[i] is not None:
+                line_start = self.position.asTuple()
+                line_end = self.neighbors[i].position.asTuple()
+                pygame.draw.line(screen, white, line_start, line_end, 4)
+                pygame.draw.circle(screen, red, self.position.asInt(), 12)
+
+class Maze(object):
+
+    def __init__(self):
+        self.nodeList = []
+
+    def setupTestNodes(self):
+        nd_A = Node(80, 80)
+        nd_B = Node(160, 80)
+        nd_C = Node(80, 40)
+        nd_D = Node(160, 80)
+
+        self.nodeList.append(
+            nd_A,
+            nd_B,
+            nd_C,
+            nd_D,
+        )
+
+        nd_A.neighbors[RIGHT] = nd_B
+        nd_B.neighbors[LEFT] = nd_A
+        nd_B.neighbors[UP] = nd_C
+        nd_B.neighbors[RIGHT] = nd_D
+        nd_C.neighbors[DOWN] = nd_B
+        nd_D.neighbors[LEFT] = nd_B
+
+    def draw (self, screen):
+        for node in self.nodeList:
+            node.draw(screen)
 
 def main():
     print("Hello World!")
